369f47b206b20325b39148ecb46022b8597e6ece
jsyx1994 <526593251@qq.com>
2019-10-22 16:24:48 +0800

    init

---
+++ mcts.h
@@ -0,0 +1,22 @@
+//
+// Created by toby on 2019/10/22.
+//
+
+#ifndef OTHELLO_TORCH_MCTS_H
+#define OTHELLO_TORCH_MCTS_H
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include "jsoncpp/json.h"
+
+
+
+#endif //OTHELLO_TORCH_MCTS_H
